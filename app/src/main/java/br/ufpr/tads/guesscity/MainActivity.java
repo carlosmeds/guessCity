3a2ffc614998c7695d3e391d0300e1ada3820640
carlosdaniel99 <cdmnkg@gmail.com>
2021-02-15 14:06:50 -0300

    Add points

--- app/src/main/java/br/ufpr/tads/guesscity/MainActivity.java
+++ app/src/main/java/br/ufpr/tads/guesscity/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int answeredQuestions = 0, correctAnswers = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +17,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickAdvance(View view) {
-        Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-        startActivity(intent);
-        finish();
+//        if (answeredQuestions == 4) {
+            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+            intent.putExtra("correctAnswers", correctAnswers);
+            startActivity(intent);
+            finish();
+//        }
+    }
+
+    public void onclickGuess(View view) {
+        answeredQuestions++;
+        correctAnswers++;
     }
 }
--- app/src/main/java/br/ufpr/tads/guesscity/ResultActivity.java
+++ app/src/main/java/br/ufpr/tads/guesscity/ResultActivity.java
@@ -3,12 +3,20 @@ package br.ufpr.tads.guesscity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+    private int correctAnswers = 4;
+    TextView textViewResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        textViewResult = findViewById(R.id.textViewResult);
+
+        Bundle b = getIntent().getExtras();
+        correctAnswers = b.getInt("correctAnswers");
+        textViewResult.setText("Você fez " + correctAnswers * 25 + " pontos!");
     }
 }
